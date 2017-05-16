46fd058da28f6a16aeedaeb9933371fb63321a6d
Rakesh Rai (IC) <rakesh_rai@apple.com>
2017-05-16 09:41:09 -0700

    Arrow alignment fix

--- src/js/components/desktop/SubLinks.js
+++ src/js/components/desktop/SubLinks.js
@@ -5,6 +5,7 @@ export default class SubLinks extends React.Component {
     render() {
             return (
 				<div id="subcomponentId" className={this.props.classnames}>
+				<div className="sublinks">Sub links</div>
 				</div>
 	)};
 }
